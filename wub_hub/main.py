8b2ffa60eb4c19b6abac1301d4bc4501cb2824d4
Filipe Oliveira <hi@fmoliveira.dev>
2024-07-01 22:29:19 -0400

    Improve production declutter

--- wub_hub/main.py
+++ wub_hub/main.py
@@ -3,6 +3,9 @@ import streamlit as st
 from rcon.source import Client
 from PIL import Image
 
+environment = os.environ.get('ENVIRONMENT') or 'development'
+show_debug = environment.lower() != 'production'
+
 rcon_server = os.environ.get('MINECRAFT_SERVER') or '127.0.0.1'
 rcon_port = os.environ.get('MINECRAFT_RCON_PORT') or 25575
 rcon_password = os.environ.get('MINECRAFT_RCON_PASSWORD')
@@ -18,18 +21,17 @@ if 'init' not in st.session_state:
   st.session_state.output = ''
 
 st.write('# wub hub')
-st.write('---')
 
-# if st.session_state.command != '':
-#   st.code(st.session_state.command.replace('\n', '\n'), language='html')
-  
-# if st.session_state.output != '':
-#   st.code(st.session_state.output.replace('\n', '\n'), language='html')
+if show_debug and st.session_state.command != '':
+  st.code(st.session_state.command.replace('\n', '\n'), language='html')
+
+if show_debug and st.session_state.output != '':
+  st.code(st.session_state.output.replace('\n', '\n'), language='html')
 
 def send_command(command):
   try:
+    st.session_state.command = command
     with Client(rcon_server, rcon_port, passwd=rcon_password) as client:
-      st.session_state.command = command
       st.session_state.output = client.run(command)
   except ConnectionError:
     st.session_state.output = "Failed!" # may show some indicator later, but now it's just best to avoid disrupting the ui
@@ -40,11 +42,11 @@ def execute_positioned(command):
 def execute_as(command):
   send_command(f'execute as {target_player} run {command}')
 
-# def cmd_help():
-#   send_command('help')
+def cmd_help():
+  send_command('help')
 
-# def cmd_list():
-#   send_command('list')
+def cmd_list():
+  send_command('list')
 
 def cmd_setblock():
   execute_positioned('setblock ~ ~ ~ minecraft:shroomlight')
@@ -55,9 +57,12 @@ def cmd_light():
 def cmd_barrier():
   execute_as('give @s minecraft:barrier')
 
-# st.button('help', on_click=cmd_help)
-# st.button('list', on_click=cmd_list)
+if show_debug:
+  with st.container():
+    st.button('help', on_click=cmd_help)
+    st.button('list', on_click=cmd_list)
 
+st.write('---')
 col1, col2, col3 = st.columns(3)
 
 with col1:
@@ -73,17 +78,20 @@ with col3:
   st.button('BARRIER', on_click=cmd_barrier)
 
 style = '''
-<style>
 /* center images and buttons in each container */
 div.element-container { text-align: center; }
 div[data-testid="stFullScreenFrame"] div { justify-content: center; }
 
-/* hide development controls + full screen button */
-.stDeployButton, #MainMenu, button[data-testid="StyledFullScreenButton"] { display: none; }
-
 /* reduce visual clutter from heading spacing */
 hr { opacity: 0; }
-</style>
 '''
 
-st.markdown(style, unsafe_allow_html=True)
+if not show_debug:
+  style += '''
+  /* hide development controls + full screen button */
+  .stDeployButton, #MainMenu, div[data-testid="stHeader"], div[data-testid="stToolbar"], div[data-testid="stStatusWidget"], button[data-testid="StyledFullScreenButton"] {
+    display: none;
+  }
+  '''
+
+st.markdown(f'<style>{style}</style>', unsafe_allow_html=True)
